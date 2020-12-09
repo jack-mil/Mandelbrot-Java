697fccbb4eb68a1bec803f9a8b6453ead71fd558
jack-mil.thonkpad <62065280+jack-mil@users.noreply.github.com>
2020-12-09 13:32:36 -0500

    rearrange and refactor methods in FractalImageView and MandelbrotImageView

--- src/mandelbrotset/FractalImageView.java
+++ src/mandelbrotset/FractalImageView.java
@@ -1,74 +1,109 @@
 package mandelbrotset;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
- * Represents a Mandelbrot fractal Image.
+ * Represents a 2D fractal Image.
  * 
  * Method render() generates internal Image object property using settings
- * defined by public Property value.
+ * defined by public Property values.
  * 
  * Only generates 800 x 800 px images at this point, resizable images are saved
  * for future.
  */
 public abstract class FractalImageView extends ImageView {
 
-    // Define the image size
-    protected final int WIDTH = 800;
-    protected final int HEIGHT = 800;
+  // Define the image size
+  protected final int WIDTH = 1200;
+  protected final int HEIGHT = 1200;
 
-    protected final IntegerProperty maxIterations;
-    protected final ObjectProperty<Color> inColor;
-    protected final ObjectProperty<Color> outColor;
-    protected final BooleanProperty psychedelic;
+  protected final IntegerProperty maxIterations;
+  protected final ObjectProperty<Color> inColor;
+  protected final ObjectProperty<Color> outColor;
+  protected final BooleanProperty psychedelic;
 
-    /** Creates a new 800x800 px ImageView Node with internal fractal image. */
-    public FractalImageView() {
+  /** Creates a new 800x800 px ImageView Node with internal fractal image. */
+  public FractalImageView() {
 
-        this.setImage(new WritableImage(WIDTH, HEIGHT));
+    this.setImage(new WritableImage(WIDTH, HEIGHT));
 
-        this.maxIterations = new SimpleIntegerProperty(this, "Iteration Count");
-        this.psychedelic = new SimpleBooleanProperty(this, "Crazy colors", false);
-        this.inColor = new SimpleObjectProperty<>(this, "inColor", Color.RED);
-        this.outColor = new SimpleObjectProperty<>(this, "outColor", Color.BLUE);
+    this.maxIterations = new SimpleIntegerProperty(this, "Iteration Count");
+    this.psychedelic = new SimpleBooleanProperty(this, "Crazy colors", false);
+    this.inColor = new SimpleObjectProperty<>(this, "inColor", Color.RED);
+    this.outColor = new SimpleObjectProperty<>(this, "outColor", Color.BLUE);
 
+  }
+
+  // Fractal Image Properties
+  public IntegerProperty iterationsProperty() {
+    return this.maxIterations;
+  }
+
+  public ObjectProperty<Color> inColorProperty() {
+    return this.inColor;
+  }
+
+  public final ObjectProperty<Color> outColorProperty() {
+    return this.outColor;
+  }
+
+  public BooleanProperty psychedelicProperty() {
+    return this.psychedelic;
+
+  }
+
+  /** Open a file chooser and save the internal fractal image */
+  public void saveImageToFile() {
+
+    // Let the user select a location to store the image
+    // Default is "home/pictures"
+    FileChooser chooser = new FileChooser();
+    chooser.setTitle("Save Image");
+    chooser.setInitialDirectory(
+        new File(System.getProperty("user.home") + System.getProperty("file.separator") + "Pictures"));
+    chooser.setInitialFileName("mandelbrot.png");
+    chooser.getExtensionFilters().add(new ExtensionFilter("Image file (*.png)", "*.png"));
+    File outFile = chooser.showSaveDialog(this.getScene().getWindow());
+
+    if (outFile != null) {
+      // Convert to an ImageBuffer we can write to a file
+      BufferedImage bImage = SwingFXUtils.fromFXImage(this.getImage(), null);
+      try {
+        ImageIO.write(bImage, "png", outFile);
+        System.out.println("File saved to " + outFile.getAbsolutePath());
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
+  }
 
-    // Fractal Image Properties
-    public IntegerProperty iterationsProperty() {
-        return this.maxIterations;
-    }
+  /**
+   * The rendering algorithm must be defined in child classes of abstract class
+   * FractalImageView
+   */
+  public abstract void render();
 
-    public ObjectProperty<Color> inColorProperty() {
-        return this.inColor;
-    }
-
-    public final ObjectProperty<Color> outColorProperty() {
-        return this.outColor;
-    }
-
-    public BooleanProperty psychedelicProperty() {
-        return this.psychedelic;
-    }
-
-    /**
-     * The rendering algorithm must be defined in child classes of abstract class
-     * FractalImageView
-     */
-    public abstract void render();
-
-    /**
-     * The abstract method pickColor is used internally by render() to determine
-     * rendered pixel colors. This must be defined my desired fractal class
-     */
-    protected abstract Color pickColor(int N);
+  /**
+   * The abstract method pickColor is used internally by render() to determine
+   * rendered pixel colors. This must be defined my desired fractal class
+   */
+  protected abstract Color pickColor(int N);
 
 }
--- src/mandelbrotset/MandelbrotImageView.java
+++ src/mandelbrotset/MandelbrotImageView.java
@@ -5,92 +5,115 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 /**
- * Represents a Mandelbrot fractal Image. 
+ * Represents a Mandelbrot fractal Image.
  * 
- * Method render() generates internal Image object property using settings 
+ * Method render() generates internal Image object property using settings
  * defined by public Property value.
  * 
  * Only generates 800 x 800 px images at this point, resizable images are saved
  * for future.
  */
 public class MandelbrotImageView extends FractalImageView {
-    
-    private double _maxIter;
-    private Color _color1;
-    private Color _color2;
 
-    /**
-     * The MandelbrotSet Object generates its pixelBuffer when it is run. Basic
-     * algorithm from: https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
-     */
-    public void render() {
-        // Use PixelWriter to edit this ImageView's internal Image pixels
-        PixelWriter pixels = ((WritableImage) this.getImage()).getPixelWriter();
+  private double _maxIter;
+  private Color _color1;
+  private Color _color2;
 
-        // Get current settings from bound properties, once per render
-        // No need to poll every iteration.
-        _maxIter = this.maxIterations.doubleValue();
-        _color1 = this.outColor.getValue();
-        _color2 = this.inColor.getValue();
+  // Values for the Mandelbrot set
+  private static double MANDELBROT_RE_MIN = -2;
+  private static double MANDELBROT_RE_MAX = 1;
+  private static double MANDELBROT_IM_MIN = -1.2;
+  private static double MANDELBROT_IM_MAX = 1.2;
 
-        for (int Py = 0; Py < HEIGHT; Py++) {
-            for (int Px = 0; Px < WIDTH; Px++) {
-                // Scale image coordinates to mandelbrot range
-                // All points are within a radius 2 circle on complex plane
-                // I chose points that made a nice centered, square image
-                // This will be point c0
-                double x0 = Px / (double) HEIGHT * 3 - 2.15;
-                double y0 = Py / (double) WIDTH * 3 - 1.5;
+  /**
+   * The MandelbrotSet Object generates its pixelBuffer when it is run. Basic
+   * algorithm from:
+   * https://en.wikipedia.org/wiki/Mandelbrot_set#Computer_drawings
+   */
+  public void render() {
+    // Use PixelWriter to edit this ImageView's internal Image pixels
+    PixelWriter pixels = ((WritableImage) this.getImage()).getPixelWriter();
 
-                // Initial complex number z = (x + iy)
-                double x = 0.0;
-                double y = 0.0;
-                // Track iterations
-                int n = 0;
+    // Get current settings from bound properties, once per render
+    // No need to poll every iteration.
+    _maxIter = this.maxIterations.doubleValue();
+    _color1 = this.outColor.getValue();
+    _color2 = this.inColor.getValue();
 
-                // |z| must remain <= 2 in every iteration for point c to be
-                // in the mandelbrot set
-                while (x * x + y * y <= 2 * 2 && n < _maxIter) {
-                    // Calculate Re(z^2 + c0)
-                    double xtemp = x * x - y * y + x0;
-                    // Calculate Im(z^2 + c0)
-                    y = 2 * x * y + y0;
-                    x = xtemp;
-                    n++;
-                }
+    for (int Py = 0; Py < HEIGHT; Py++) {
+      for (int Px = 0; Px < WIDTH; Px++) {
+        // Scale image coordinates to mandelbrot range
+        // All points are within a radius 2 circle on complex plane
+        // I chose points that made a nice centered, square image
+        // This will be point c0
+        double x0 = Px / (double) HEIGHT * 3 - 2.15;
+        double y0 = Py / (double) WIDTH * 3 - 1.5;
 
-                pixels.setColor(Px, Py, pickColor(n));
-            }
-        }
+        int steps = checkConvergence(x0, y0);
+
+        pixels.setColor(Px, Py, pickColor(steps));
+      }
     }
+  }
 
-    /**
-     * Set the hue of each pixel based on the number of iterations completed. The
-     * range is set by user input. Two modes are given: Normal mode is a gradient
-     * from c1 -> c2 Psychedelic produces random looking well defined bands of color
-     * 
-     * The pixel will be black if it is determined to be *in* the set
-     * 
-     * @param N The iteration count to calculate color for
-     * @return
-     */
-    protected Color pickColor(int N) {
+  /** The actual Mandelbrot algorithm implementation.
+   * Checks whether f(Z)=Z² + C converges when iterated from z=0;
+   * where Z and C are complex numbers
+   * @param c real part of complex number
+   * @param ci imaginary part
+   * @return the number of iterations before convergence. n <= maxIter
+   */
+  private int checkConvergence(double c, double ci) {
+    // Initial complex number Z = (z + zi)
+    double z = 0.0;
+    double zi = 0.0;
+    // Track iterations
+    int n = 0;
 
-        double hue;
-        double value;
-        double sat;
-        
-        if (this.psychedelic.getValue()) {
-            hue = _color1.getHue() * (_maxIter / N);
-            value = (N < _maxIter) ? 1.0 : 0.0;
-            sat = _color1.getSaturation();
-        } else {
-            hue = _color2.getHue() * (N / _maxIter) + _color1.getHue() * (_maxIter - N) / _maxIter;
-            value = (N < _maxIter) ? (_color1.getBrightness() + _color2.getBrightness()) / 2.0 : 0.0;
-            sat = (_color1.getSaturation() + _color2.getSaturation()) / 2.0;
-        }
-        return Color.hsb(hue, sat, value);
+    // |Z| must remain <= 2 in every iteration for point c to be
+    // in the mandelbrot set
+    while (z * z + zi * zi <= 4.0 && n <= _maxIter) {
+      // Calculate Re(z^2 + c0)
+      double zT = z * z - zi * zi + c;
+      // Calculate Im(z^2 + c0)
+      double ziT = 2 * z * zi + ci;
+      zi = ziT;
+      z = zT;
+      n++;
     }
+    return n;
+  }
 
+  /**
+   * Set the hue of each pixel based on the number of iterations completed. The
+   * range is set by user input. Two modes are given: Normal mode is a gradient
+   * from c1 -> c2 Psychedelic produces random looking well defined bands of color
+   * 
+   * The pixel will be black if it is determined to be *in* the set
+   * 
+   * @param N The iteration count to calculate color for
+   * @return javafx.scene.paint.Color
+   */
+  protected Color pickColor(int N) {
+
+    double hue;
+    double value;
+    double sat;
+
+    // Math is a bit ad-hoc for what looked good to me. Basically a function
+    // of convergence speed, i.e. the ratio between max iterations and actual
+    // iterations
+    // for this pixel
+    if (this.psychedelic.getValue()) {
+      hue = _color1.getHue() * (_maxIter / N);
+      value = (N < _maxIter) ? 1.0 : 0.0;
+      sat = _color1.getSaturation();
+    } else {
+      hue = _color2.getHue() * (N / _maxIter) + _color1.getHue() * (_maxIter - N) / _maxIter;
+      value = (N < _maxIter) ? (_color1.getBrightness() + _color2.getBrightness()) / 2.0 : 0.0;
+      sat = (_color1.getSaturation() + _color2.getSaturation()) / 2.0;
+    }
+    return Color.hsb(hue, sat, value);
+  }
 
 }
